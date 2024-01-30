ec0db79c9fa58ef91a5248b793361cd4afeeaaa5
Joyrudra Biswas <joyrudra@itobuz.com>
2024-01-30 13:30:52 +0530

    feat : added support for exiting the app

--- src/components/global/ButtonsLinks/BackButton.tsx
+++ src/components/global/ButtonsLinks/BackButton.tsx
@@ -1,20 +1,25 @@
-import {StyleSheet, TouchableOpacity} from 'react-native';
+import {BackHandler, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
 import BackLogo from '../../../../assets/images/icons/backLogo';
 import {useNavigation} from '@react-navigation/native';
 
 export default function BackButton({
   location,
-}: {
+}: Readonly<{
   location?: string | undefined;
-}) {
+}>) {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity
       style={[style.buttonStyle]}
       onPress={() => {
-        location ? navigation.navigate(location) : navigation.goBack();
+        if (navigation.canGoBack()) {
+          location ? navigation.navigate(location) : navigation.goBack();
+          return;
+        }
+
+        BackHandler.exitApp();
       }}>
       <BackLogo />
     </TouchableOpacity>
